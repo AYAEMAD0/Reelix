e4aaff4f95b11be4de7df3fd2c322e63eb1db3d9
Aya Emad <ayaelbaroudy0@gmail.com>
2025-10-06 00:28:36 +0300

    + integration firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:easy_localization/easy_localization.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'core/routing/app_router.dart';
+import 'firebase_options.dart';
 import 'reelix.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     EasyLocalization(
       supportedLocales: [Locale('en'), Locale('ar')],
